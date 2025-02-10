19987cbdcc6b5849e7a0e12004c9c72dd3be6c95
mohamad8616 <mohamad.sh.a9036@gmail.com>
2025-02-10 23:51:15 +0330

    added the navbar component but is has some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import { useState } from "react";
 
-function App() {
-  return <p>hi</p>;
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 }
-
-export default App;
+function Navbar() {
+  return (
+    <header>
+      <div>
+        <p>
+          این یک برنامه برنامه روزانه قدرتمند و کارا است. شما می توانید از این
+          برنامه جهت استفاده روزانه بهره ببرید
+        </p>
+      </div>
+      <div className='img-container'>
+        <img src='./src/todo.svg' alt='todolist' />
+      </div>
+      <nav>
+        <ul className='list-container'>
+          <li className='list-item'>اطلاعات بیشتر</li>
+          <li className='list-item'>درباره ما</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
